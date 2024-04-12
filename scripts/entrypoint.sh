e3f67cbd0dbd2d88d60bf67b80cc1e0e8c4971bd
lenkan <dlenksjo@gmail.com>
2024-04-12 11:56:58 +0200

    add auth env

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$AUTH" ]; then
+    echo "No auth"
+    exit 1;
+fi;
+
 kli init --name sally --nopasscode --config-dir ./scripts --config-file docker-oobis
 kli incept --name sally --alias sally --file ./scripts/data/local.json
-sally server start --name sally --alias sally --web-hook http://sally-demo:9923 --auth EHOuGiHMxJShXHgSb6k_9pqxmRb8H-LT0R2hQouHp8pW
+
+sally server start --name sally --alias sally --web-hook http://sally-demo:9923 --auth "$AUTH"
