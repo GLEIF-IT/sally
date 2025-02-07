915feb995ad31e47255ea0fa722ba8c98fb82fec
Kent Bull <kent@kentbull.com>
2025-02-07 15:06:47 -0700

    docs: update readme docs; remove unneeded files

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+# Example start command
+# Warning: Use your own salt and passcode. Do not use the example values below.
 
-sally server start --name sally --alias sally --web-hook http://127.0.0.1:9923 --auth EHOuGiHMxJShXHgSb6k_9pqxmRb8H-LT0R2hQouHp8pW
+# This will use the config file located at ./scripts/keri/cf/sally.json
+sally server start \
+  --name sally \
+  --alias sally \
+  --salt 0AD3GmJxuDbCs90qC0ipHfOD \
+  --passcode bWuSoMabM6CSSJUqc97YS \
+  --web-hook http://127.0.0.1:9923 \
+  --auth EHOuGiHMxJShXHgSb6k_9pqxmRb8H-LT0R2hQouHp8pW \
+  --config-dir scripts \
+  --config-file sally.json \
+  --loglevel INFO
